6c3b71dc5db0b4feeecb376cf6a531af268647ee
Denmark-Banana <neat507kr@gmail.com>
2020-02-20 11:03:35 +0900

    add TopChart > Paper hover css

--- src/components/TopChart.js
+++ src/components/TopChart.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
     padding: theme.spacing(3),
     //background: '#eeeeee',
     display: 'flex',
-    justifyContent: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   paper: {
     padding: theme.spacing(3),
     textAlign: 'center',
-    //color: theme.palette.text.primary,
-    color: theme.palette.common.red,
+    color: theme.palette.text.primary,
     marginLeft: 15,
     marginRight: 15,
-    minWidth: 200
+    minWidth: 200,
+    "&:hover": {
+      cursor: 'pointer',
+      boxShadow: '0 2px 3px 1px gray'
+    },
   },
 });
 
@@ -30,19 +33,6 @@ class TopChart extends Component {
       return null;
     }
     return (
-      // <div className={classes.root}>
-      //   <Grid container spacing={3}>
-      //     {tickers.map(([key, ticker]) => (
-      //       <Grid item xs={2} key={key}>
-      //         <Paper className={classes.paper}>
-      //           {key}
-      //           <br></br> {ticker.max_price} 원
-      //           <br></br> {ticker.fluctate_rate_24H} %
-      //         </Paper>
-      //       </Grid>
-      //     ))}
-      //   </Grid>
-      // </div>
       <div className={classes.root}>
         {tickers.map(([key, ticker]) => (
           <Paper className={classes.paper} key={key}>
